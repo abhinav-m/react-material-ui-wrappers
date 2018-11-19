74043d036d2f60a8f1c68f7e30ab9a0551eca82d
Abhinav Mishra <abhinav.thinktank@gmail.com>
2018-11-19 11:09:06 +0530

    Started writing base example of textfield

--- src/components/wrappers/forms/TextField.js
+++ src/components/wrappers/forms/TextField.js
@@ -3,5 +3,15 @@ import PropTypes from "prop-types"
 
 
 class TextField extends Component {
-    constructor
-}
+    static propTypes = {
+        style:PropTypes.object, //Style passed to the text field
+        value:PropTypes.string, //Value of the text field
+        floatingLabelText:PropTypes.string, //Material ui floating label text for the input
+        onFocusOut:PropTypes.func,// callback to be executed on focusing out of component
+        type:PropTypes.string,//DOM input field type
+        name:PropTypes.string.isRequired,//DOM input field name (required for setting the value of the component through postvalidation)
+        postValidation:PropTypes.func.isRequired,//callback to execute post validation of the component
+        trim:PropTypes.bool,//Whether to trim the value of the component
+        regex:PropTypes.
+    }
+}   
